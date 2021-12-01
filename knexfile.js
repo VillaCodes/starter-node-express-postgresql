676e1eeff9942d8bad946c37e81ad6d423af7e1d
Johnathan Villa Castruita <88985846+VillaCodes@users.noreply.github.com>
2021-12-01 03:16:11 -0800

    Created service files
    updated with service files

--- knexfile.js
+++ knexfile.js
@@ -5,9 +5,12 @@ const { DATABASE_URL } = process.env;
 module.exports = {
   development: {
     client: "postgresql",
-    connection: "DATABASE_URL",
+    connection: "postgres://vhegzdey:kd4Vrz34V4WB-KA-IMmgkhKlLZFWt5DJ@kashin.db.elephantsql.com/vhegzdey",
     migrations: {
     directory: path.join(__dirname, "src", "db", "migrations"),
     },
+    seeds: {
+          directory: path.join(__dirname, "src", "db", "seeds"),
+         }
   },
 };
